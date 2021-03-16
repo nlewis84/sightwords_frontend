997b7a9dd04d44884822b34e84f1e3accea77bb7
nllew <n.lewis.perc@gmail.com>
2021-03-15 23:31:32 -0500

    display words and spanize them

--- src/game.js
+++ src/game.js
@@ -1,18 +1,45 @@
+// initial state
+let timeElapsed = 0;
+let total_errors = 0;
+let errors = 0;
+let wordId = 0;
+
 function playGame(list) {
 
     const gameList = list
     gameList.listWords().forEach(e => {
+
         addAudio(e);
-        addTextInput(e);
+        addText(e);
     })
 }
 
 function addAudio(wordObj) {
     let audio = wordObj.renderAudio();
-    // 
     document.querySelector('#list-container').innerHTML = audio;
 }
 
-function addTextInput(wordObj) {
+function addText(wordObj) {
 
+    const newDiv = document.createElement("div");
+    newDiv.className = "word-box";
+
+    const body = document.querySelector('body');
+    body.append(newDiv);
+
+    let characterized = wordObj.characterize()
+
+    spanized(characterized)
+
+    // newDiv.innerText = wordObj.characterize()
+
+}
+
+function spanized(array) {
+    array.forEach(char => {
+        const wordDiv = document.body.lastElementChild;
+        const charSpan = document.createElement('span')
+        charSpan.innerText = char
+        wordDiv.appendChild(charSpan)
+    })
 }
