a296e4b68a34d4f5b1aa411138d0ccdf16ec8785
nllew <n.lewis.perc@gmail.com>
2021-03-16 19:06:41 -0500

    working display of audio and word

--- src/game.js
+++ src/game.js
@@ -4,42 +4,71 @@ let total_errors = 0;
 let errors = 0;
 let wordId = 0;
 
+
+
 function playGame(list) {
 
     const gameList = list
     gameList.listWords().forEach(e => {
 
-        addAudio(e);
-        addText(e);
+        // addAudio(e);
+        // addText(e);
+        addWordBox(e);
     })
 }
 
-function addAudio(wordObj) {
-    let audio = wordObj.renderAudio();
-    document.querySelector('#list-container').innerHTML = audio;
-}
-
-function addText(wordObj) {
-
+function addWordBox(wordObj) {
+    const container = document.querySelector('#list-container');
     const newDiv = document.createElement("div");
     newDiv.className = "word-box";
+    container.appendChild(newDiv);
+    let audio = wordObj.renderAudio();
 
-    const body = document.querySelector('body');
-    body.append(newDiv);
+    container.lastElementChild.innerHTML = audio
 
-    let characterized = wordObj.characterize()
+    let characterized = wordObj.characterize();
 
-    spanized(characterized)
+    spanized(characterized);
 
-    // newDiv.innerText = wordObj.characterize()
+    // newDiv.innerText += spanWord;
 
 }
+
+// function addAudio(wordObj) {
+// const container = document.querySelector('#list-container');
+// const newDiv = document.createElement("div");
+// newDiv.className = "word-box";
+// container.appendChild(newDiv);
+// let audio = wordObj.renderAudio();
+// document.querySelector('#list-container').innerHTML = audio;
+// }
+
+// function addText(wordObj) {
+
+//     const newDiv = document.createElement("div");
+//     newDiv.className = "word-box";
+
+//     const body = document.querySelector('body');
+//     body.append(newDiv);
+
+//     let characterized = wordObj.characterize()
+
+//     spanized(characterized)
+
+//     // newDiv.innerText = wordObj.characterize()
+
+// }
 
 function spanized(array) {
     array.forEach(char => {
-        const wordDiv = document.body.lastElementChild;
+        const container = document.querySelector('#list-container');
+        const newDiv = document.createElement("div");
+        // const wordDiv = document.body.lastElementChild;
         const charSpan = document.createElement('span')
+
         charSpan.innerText = char
-        wordDiv.appendChild(charSpan)
+        container.lastElementChild.appendChild(charSpan)
+        // newDiv.appendChild(charSpan)
+
     })
 }
