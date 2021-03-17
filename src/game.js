bd8e051ad6ace6d7ba0d36cbf5fa5799fcc16cce
nllew <n.lewis.perc@gmail.com>
2021-03-16 19:07:50 -0500

    removed old comments

--- src/game.js
+++ src/game.js
@@ -10,9 +10,6 @@ function playGame(list) {
 
     const gameList = list
     gameList.listWords().forEach(e => {
-
-        // addAudio(e);
-        // addText(e);
         addWordBox(e);
     })
 }
@@ -23,52 +20,18 @@ function addWordBox(wordObj) {
     newDiv.className = "word-box";
     container.appendChild(newDiv);
     let audio = wordObj.renderAudio();
-
     container.lastElementChild.innerHTML = audio
-
     let characterized = wordObj.characterize();
-
     spanized(characterized);
-
-    // newDiv.innerText += spanWord;
-
 }
-
-// function addAudio(wordObj) {
-// const container = document.querySelector('#list-container');
-// const newDiv = document.createElement("div");
-// newDiv.className = "word-box";
-// container.appendChild(newDiv);
-// let audio = wordObj.renderAudio();
-// document.querySelector('#list-container').innerHTML = audio;
-// }
-
-// function addText(wordObj) {
-
-//     const newDiv = document.createElement("div");
-//     newDiv.className = "word-box";
-
-//     const body = document.querySelector('body');
-//     body.append(newDiv);
-
-//     let characterized = wordObj.characterize()
-
-//     spanized(characterized)
-
-//     // newDiv.innerText = wordObj.characterize()
-
-// }
 
 function spanized(array) {
     array.forEach(char => {
         const container = document.querySelector('#list-container');
         const newDiv = document.createElement("div");
-        // const wordDiv = document.body.lastElementChild;
         const charSpan = document.createElement('span')
 
         charSpan.innerText = char
         container.lastElementChild.appendChild(charSpan)
-        // newDiv.appendChild(charSpan)
-
     })
 }
