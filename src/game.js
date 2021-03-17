a225728f75a9708e578331de71cde1f2f9b4ec9b
nllew <n.lewis.perc@gmail.com>
2021-03-16 19:38:38 -0500

    input styling

--- src/game.js
+++ src/game.js
@@ -15,14 +15,16 @@ function playGame(list) {
 }
 
 function addWordBox(wordObj) {
+    let characterized = wordObj.characterize();
+    spanized(characterized);
     const container = document.querySelector('#list-container');
     const newDiv = document.createElement("div");
     newDiv.className = "word-box";
     container.appendChild(newDiv);
     let audio = wordObj.renderAudio();
     container.lastElementChild.innerHTML = audio
-    let characterized = wordObj.characterize();
-    spanized(characterized);
+    addInput(wordObj);
+
 }
 
 function spanized(array) {
@@ -32,13 +34,21 @@ function spanized(array) {
     container.appendChild(newDiv);
 
     array.forEach(char => {
-
-
         const charSpan = document.createElement('span')
-
-
-
         charSpan.innerText = char
         container.lastElementChild.appendChild(charSpan)
     })
+}
+
+function addInput(e) {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.id = e.id;
+    input.size = "10";
+
+    const container = document.querySelector('#list-container');
+    const newDiv = document.createElement("div");
+    newDiv.className = "word-input";
+    container.appendChild(newDiv);
+    container.lastElementChild.appendChild(input);
 }
